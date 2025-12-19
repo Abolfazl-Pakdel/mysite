61b5b5143c64072f2f33343713dd6e8ea295e883
Abolfazl-Pakdel <abolfazlpakdelpour@gmail.com>
2025-12-19 23:40:33 +0330

    fixed all
    the description of blog-home was not fixed good so it just need </div> after {endfor}

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,9 @@ from blog.models import Post
 
 class PostAdmin(admin.ModelAdmin):
   date_hierarchy = 'created_date'
-  empty_value_display = '<-empty->'
+  empty_value_display = 'empty'
   list_display = ('title','counted_view', 'status', 'published_date',)
   list_filter = ('status',)
-  ordering = ['-created_date']
+  # ordering = ['-created_date']
   search_fields = ['title', 'content']
 admin.site.register(Post, PostAdmin)
