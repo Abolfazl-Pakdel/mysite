33319791526cdf95e32f0928d0d4b9f093c25fda
Abolfazl-Pakdel <abolfazlpakdelpour@gmail.com>
2025-12-28 17:50:31 +0330

    learn new thing
    i wanted to add max-width in main.css but when i added this the changes did not appear on site so i search and i found that this is a cache of browser soo if you want to clear the cache on your browser just do this on browser
    CTRL + F5
    :)

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,3 +1,27 @@
 from django import template
-
+from blog.models import Post
 register = template.Library()
+
+@register.simple_tag(name="totalposts")
+def function(a = 5):
+    post = Post.objects.filter(status=1).count()
+    return post
+
+@register.simple_tag(name="posts")
+def function(a = 5):
+    post = Post.objects.filter(status=1)
+    return post
+
+@register.filter()
+def snippet(value, arg=20):
+    return value[:arg] + '...'
+
+# @register.inclusion_tag('popularposts.html')
+# def popularposts():
+#     posts = Post.objects.filter(status=1).order_by('published_date')
+#     return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts': posts}
--- blog/views.py
+++ blog/views.py
@@ -16,9 +16,9 @@ def blog_single(request, pid):
   context = {'post': post,'previous_post': previous_post,'next_post': next_post}
   return render(request, 'blog/blog-single.html', context)
 
-def test(request, pid):
+def test(request):
   # posts = Post.objects.all()
   # post = Post.objects.get(id=pid)
-  post = get_object_or_404(Post, pk=pid)
-  context = {'post': post}
-  return render(request, 'test.html', context)
+  # post = get_object_or_404(Post, pk=pid)
+  # context = {'post': post}
+  return render(request, 'test.html')
